40efc9f1ae761cf4d17b12d0e115b4439238ec90
masamichhhhi <masamichhhi@gmail.com>
2021-12-25 22:21:26 +0900

    impl crop functions

--- crop.go
+++ crop.go
@@ -1,11 +1,66 @@
 package p
 
 import (
+	"context"
+	"image"
+	"io"
 	"net/http"
 	"strconv"
+	"strings"
+
+	"cloud.google.com/go/storage"
+	"github.com/disintegration/imaging"
+	"google.golang.org/api/option"
 )
 
 func CropImage(w http.ResponseWriter, r *http.Request) {
+	width, height, cropStartX, cropStartY, err := ParseCropParams(r)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	credentialFilePath := "./key.json"
+	bucketName := "image-cropper-source"
+
+	objectPath := strings.Replace(r.URL.Path, "/", "", 1)
+
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx, option.WithCredentialsFile(credentialFilePath))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	obj := client.Bucket(bucketName).Object(objectPath)
+
+	reader, err := obj.NewReader(ctx)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer reader.Close()
+
+	cropParams := NewCropParams(width, height, cropStartX, cropStartY)
+
+	img, err := Crop(reader, cropParams)
+	encoded, err := EncodeImageToJpg(img)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.Header().Set("Content-Length", strconv.Itoa(encoded.Len()))
+
+	_, err = io.Copy(w, encoded)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 }
 
@@ -29,4 +84,24 @@ func ParseCropParams(r *http.Request) (int, int, int, int, error) {
 	cropStartY, _ := strconv.Atoi(query.Get("cropStartY"))
 
 	return width, height, cropStartX, cropStartY, nil
+}
+
+func Crop(r io.Reader, params CropParams) (*image.Image, error) {
+	var dst image.Image
+
+	src, _, err := image.Decode(r)
+	if err != nil {
+		return &dst, err
+	}
+
+	if params.width == 0 && params.height == 0 {
+		dst = src
+	} else {
+		dst = imaging.Crop(src, image.Rectangle{
+			Min: image.Point{X: params.cropStartX, Y: params.cropStartY},
+			Max: image.Point{X: params.cropStartX + params.width, Y: params.cropStartY + params.height},
+		})
+	}
+
+	return &dst, nil
 }
